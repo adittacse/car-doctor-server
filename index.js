068f0d786d4aaa294d7e9c2db79815d5750ff315
Aditta Chakraborty <adittacse@gmail.com>
2023-05-17 02:57:23 +0600

    booking order status confirmed send to database

--- index.js
+++ index.js
@@ -13,79 +13,96 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const serviceCollection = client.db("cardoctor").collection("services");
-    const bookingCollection = client.db("cardoctor").collection("bookings");
+        const serviceCollection = client.db("cardoctor").collection("services");
+        const bookingCollection = client.db("cardoctor").collection("bookings");
 
-    // step-1: get all services data from mongodb
-    app.get("/services", async (req, res) => {
-        const cursor = serviceCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    });
+        // step-1: get all services data from mongodb
+        app.get("/services", async (req, res) => {
+            const cursor = serviceCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
 
-    // step-2: get specific id's information of service
-    app.get("/services/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
-        const result = await serviceCollection.findOne(query);
-        res.send(result);
-    });
+        // step-2: get specific id's information of service
+        app.get("/services/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await serviceCollection.findOne(query);
+            res.send(result);
+        });
 
-    // bookings
+        // bookings
 
-    // step-2: get all booking data based on logged user from mongodb
-    app.get("/bookings", async (req, res) => {
-        let query = {};
-        if (req.query?.email) {
-            query = { email: req.query.email };
-        }
-        const cursor = bookingCollection.find(query);
-        const result = await cursor.toArray();
-        res.send(result);
-    });
+        // step-2: get all booking data based on logged user from mongodb
+        app.get("/bookings", async (req, res) => {
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email };
+            }
+            const cursor = bookingCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
 
-    // step-1: inserting booking data from client side to mongodb
-    app.post("/bookings", async (req, res) => {
-        const booking = req.body;
-        const result = await bookingCollection.insertOne(booking);
-        res.send(result);
-    });
+        // step-1: inserting booking data from client side to mongodb
+        app.post("/bookings", async (req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        });
 
-    // step-3: delete a booking
-    app.delete("/bookings/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const result = await bookingCollection.deleteOne(query);
-        res.send(result);
-    });
+        // step-4: updating a booking
+        app.patch("/bookings/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatedBooking = req.body;
+            const updateDoc = {
+                $set: {
+                    status: updatedBooking.status
+                },
+            };
+            const result = await bookingCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        });
+
+
+        // step-3: delete a booking
+        app.delete("/bookings/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Car Doctor is checking health!');
+    res.send('Car Doctor is checking health!');
 });
 
 app.listen(port, () => {
-  console.log(`Car Doctor is running on port ${port}`);
+    console.log(`Car Doctor is running on port ${port}`);
 });
