8559410fcf65fb6920a27752523f76f4975dc769
Aditta Chakraborty <adittacse@gmail.com>
2023-05-18 01:47:24 +0600

    implemented token verify

--- index.js
+++ index.js
@@ -21,6 +21,21 @@ const client = new MongoClient(uri, {
     }
 });
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: "Unauthorized Access" });
+    }
+    const token = authorization.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+            return res.status(403).send({ error: true, message: "Unauthorized Access"});
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -57,7 +72,7 @@ async function run() {
         // bookings routes
 
         // step-2: get all booking data based on logged user from mongodb
-        app.get("/bookings", async (req, res) => {
+        app.get("/bookings", verifyJWT, async (req, res) => {
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email };
